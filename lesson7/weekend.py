6f9b000de1758d16029c462bb38b851dadb5fac2
Tony Hung <chenyuan_hung@berkeley.edu>
2020-12-25 14:11:42 -0800

    Create weekend.py

---
+++ lesson7/weekend.py
@@ -0,0 +1,16 @@
+
+def weekend(day):
+    # your code here
+    if day=="Saturday" or day=="Sunday":
+        return True
+    else:
+        return False
+    
+print weekend('Monday')
+#>>> False
+
+#print weekend('Saturday')
+#>>> True
+
+#print weekend('July')
+#>>> False
